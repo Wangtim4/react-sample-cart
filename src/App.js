14f8a3df1cf6d0c3304536209ca4ac19ffd7757c
a2453540 <a2453540@gmail.com>
2023-05-17 13:06:34 +0800

    登入頁面

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import { Routes, Route } from "react-router-dom";
+import Login from "./pages/login";
+import Dashboard from "./pages/admin/Dashboard";
+import  AdminProducts from "./pages/admin/AdminProducts"
+
+function App() {
+    
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/admin" element={<Dashboard />}>
+          <Route path="products" element={<AdminProducts/>}></Route>
+        </Route>
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
